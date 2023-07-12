bcfd1bb8590fb6f6f89080d3e9bfc1fb62b27de6
David <git@davidfister.de>
2023-07-13 01:47:30 +0200

    worked on the card effect

--- srcipt.js
+++ srcipt.js
@@ -1,21 +1,21 @@
 const card = document.querySelector('.card'); 
 
 
-function rotate3D(event) {
+function rotateCard(event) {
   const cardRect = card.getBoundingClientRect();
+  
   const cardCenterX = cardRect.left + cardRect.width / 2;
   const cardCenterY = cardRect.top + cardRect.height / 2;
 
-  const targetX = event.clientX;
-  const targetY = event.clientY;
 
-  const deltaX = targetX - cardCenterX;
-  const deltaY = targetY - cardCenterY;
+  const deltaX =  event.clientX - cardCenterX;
+  const deltaY = event.clientY - cardCenterY;
 
-  const angleX = deltaY * 0.01;
-  const angleY = -deltaX * 0.005;
-
-  card.style.transform = `perspective(500px) rotateX(${angleX}deg) rotateY(${angleY}deg)`;
+  const angleX = deltaY / (window.innerHeight/ 2) * 25;
+  const angleY = -deltaX / (window.innerWidth/ 2) * 25;
+  console.log(angleX);
+  document.querySelector('.card').style.setProperty("--rotateX", angleX + "deg");
+  document.querySelector('.card').style.setProperty("--rotateY", angleY + "deg");
 }
 
-document.addEventListener('mousemove', rotate3D);
+document.addEventListener('mousemove', rotateCard);
