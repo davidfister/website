857c533612069fbeeed6b7cf9fa4a14a76c3b872
David <git@davidfister.de>
2023-07-14 18:30:07 +0200

    added text, mouse and changing background

--- srcipt.js
+++ srcipt.js
@@ -1,5 +1,5 @@
 const card = document.querySelector('.card'); 
-
+const cursor = document.querySelector('.cursor');
 
 function rotateCard(event) {
   const cardRect = card.getBoundingClientRect();
@@ -13,10 +13,32 @@ function rotateCard(event) {
 
   const angleX = deltaY / (window.innerHeight/ 2) * 25;
   const angleY = -deltaX / (window.innerWidth/ 2) * 25;
-  console.log(angleX);
-  document.querySelector('.card').style.setProperty("--rotateX", -1 * angleX + "deg");
-  document.querySelector('.card').style.setProperty("--rotateY", -1 * angleY + "deg");
+  card.style.setProperty("--rotateX", -1 * angleX + "deg");
+  card.style.setProperty("--rotateY", -1 * angleY + "deg");
+}
+
+function moveCursor(event) {
+  setTimeout(
+    function () {
+      cursor.style.left = (event.clientX)-8 + "px";
+      cursor.style.top = (event.clientY)-8 + "px";
+    }, 30);
+}
+
+
+function changeColor(event) {
+  
+  Math.atan2(event.clientY - (window.innerHeight / 2) , event.clientX - (window.innerWidth / 2)) * 180 / Math.PI;
+  
+  
+  var body = document.querySelector('body');
+  
+  var deg = Math.atan2(event.clientY - (window.innerHeight / 2) , event.clientX - (window.innerWidth / 2)) * 180 / Math.PI + 90
+  
+  body.style.setProperty("--deg", deg +"deg");
 }
 
 
 document.addEventListener('mousemove', rotateCard);
+document.addEventListener('mousemove', moveCursor);
+document.addEventListener('mousemove', changeColor);
